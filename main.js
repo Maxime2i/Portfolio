40240743e5081a0178bfb57adde82742089d0b7f
Maxime Langlois <maxime.lngls21@gmail.com>
2024-02-14 20:04:30 +0100

    add animate model

--- main.js
+++ main.js
@@ -2,46 +2,58 @@ import * as THREE from 'three'
 import { GLTFLoader } from 'GLTFLoader'
 import { FBXLoader } from 'FBXLoader'
 
-var canvas = document.getElementById('myCanvas');
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000);
-camera.position.x = 0.2;
-camera.position.y = 1.6;
-camera.position.z = 0.5;
-var renderer = new THREE.WebGLRenderer({ canvas: canvas });
-renderer.setSize(canvas.width, canvas.height);
-var loader = new GLTFLoader();
+var canvas = document.getElementById('myCanvas')
+var scene = new THREE.Scene()
+var camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 1000)
+camera.position.x = 0.15
+camera.position.y = 1.65
+camera.position.z = 0.4
+var renderer = new THREE.WebGLRenderer({ canvas: canvas })
+renderer.setSize(canvas.width, canvas.height)
+const pointLight = new THREE.PointLight(0xffffff, 10)
 
-// Chargement du modèle et de l'animation
-loader.load(
-    '/img/avatar.glb',
-    function (gltf) {
-        var model = gltf.scene;
-        scene.add(model);
+pointLight.position.set(0, 5, 5)
 
-        // Création d'un mixer pour gérer l'animation
-        var mixer = new THREE.AnimationMixer(model);
+scene.add(pointLight)
 
-        // Récupération de l'animation depuis le fichier GLTF
-        var clips = gltf.animations;
 
-        // Ajout de l'animation au mixer
-        clips.forEach(function (clip) {
-            mixer.clipAction(clip).play();
-        });
+const loader = new GLTFLoader()
+let mixer
 
-        // Fonction d'animation
-        function animate() {
-            requestAnimationFrame(animate);
-            mixer.update(0.0167); // Temps écoulé entre deux trames en secondes (60 FPS)
-            renderer.render(scene, camera);
-        }
+loader.load('./img/avatar2.glb',
+  function (gltf) {
 
-        // Appel de la fonction d'animation
-        animate();
-    },
-    undefined,
-    function (error) {
-        console.error(error);
-    }
-);
+    const model = gltf.scene
+    const animations = gltf.animations
+
+    mixer = new THREE.AnimationMixer(model)
+    mixer.clipAction(animations[0]).play() 
+
+    scene.add(model)
+  })
+
+
+
+  const clock = new THREE.Clock()
+let lastElapsedTime = 0 
+
+
+function animate() {
+
+  renderer.render(scene, camera)
+
+
+  const elapsedTime = clock.getElapsedTime()
+  const deltaTime = elapsedTime - lastElapsedTime
+  lastElapsedTime = elapsedTime
+
+
+  if (mixer != undefined) {
+    mixer.update(deltaTime)
+  }
+
+  requestAnimationFrame(animate)
+}
+animate()
+
+
